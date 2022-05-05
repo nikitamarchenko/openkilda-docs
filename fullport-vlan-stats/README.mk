9fe51a130fc8afdc6dcc2510c3709d99b1e12666
Nikita Marchenko <nikita.marchenko@gmail.com>
2022-05-05 13:51:41 +0300

    full port stats mk2

--- fullport-vlan-stats/README.mk
+++ fullport-vlan-stats/README.mk
@@ -1,8 +1,70 @@
+---
+theme: gaia
+_class: lead
+backgroundColor: #fff
+backgroundImage: url('https://marp.app/assets/hero-background.svg')
+---
+
+![bg left:40% 80%](https://www.open-kilda.org/wp-content/uploads/2018/10/OpenKilda@3x.png)
+
 # Feature Proposal:
-# Gather flow statistics per VLAN selected by a user
+
+#### Gather flow statistics per VLAN selected by a user 
+
+Version 2
 
 ---
 
+## API : Create
+
+### Specify selected VLANs by flow create
+
+`POST https://{host}/flows/{flow_id}/`
+```json
+{
+  "flow_id": "f12345678",
+  "statistics": {
+  	"vlan": [3, 4, n]
+  }
+  ...
+}
+```
+
+---
+
+## API : Patch
+
+### Modify VLANs by Patch
+
+`PATCH https://{host}/flows/{flow_id}/`
+```json
+{
+  "flow_id": "f12345678",
+  "statistics": {
+  	"vlan": [5, 6] <- New VLANs 
+  }
+  ...
+}
+```
+
+---
+
+## API : Delete
+
+### Delete VLANs by Patch
+
+`PATCH https://{host}/flows/{flow_id}/`
+```json
+{
+  "flow_id": "f12345678",
+  "statistics": {
+  	"vlan": [] <- clean
+  }
+  ...
+}
+```
+
+---
 ## Full port flow scenario : Current rules
 ![Table_Pre Ingress Current](current_table_pre_ingress.png)
 
@@ -13,32 +75,22 @@
 ![Table_Pre Ingress New](new_table_pre_ingress.png)
 
 ---
+## Affected components
 
-## VLAN flow scenario : Current rules
-![Table Ingress Current](current_table_ingress.png)
-
----
-
-## VLAN flow scenario : Proposed rules
-
-![Table Ingress new](new_table_ingress.png)
-
-
-CONS: Probably we need shared meter here
+- NB
+- Flow CRUD + Sync
+- Statistics collection and store
+- DB layer
+- Floodlight
+- Production: grafana dashboards
+- GUI
 
 ---
 
-## Proposal: Create the third table dedicated to statistics
+<!-- _class: lead -->
 
-![Table Stats new](new_table_stats.png)
+# Thank you
+
+![](https://cdn.quotesgram.com/img/49/85/901176278-Powerpoint-Presentation-Jokes-1.gif)
 
 
-CONS: Need additional check if we have the ability to do that
-
----
-
-## Questions:
-
-- Should we provide an API for getting those statistics for a user?
-- Do we need stats for VLAN flows?
-- What do you think about a dedicated table for this feature?
